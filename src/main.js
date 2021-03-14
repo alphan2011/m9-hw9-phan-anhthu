7e7a6bf5fa7825b7e2854885ff8abc0685aeb653
Anh-Thu Phan <aphan@ufl.edu>
2021-03-13 19:21:23 -0700

    Create subclass IceCream

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ class Desserts {
         this.calories = calories;
     }
 }
+
+class IceCream extends Desserts {
+    constructor(type, calories, flavor, scoops) {
+        super(type, calories);
+        this.flavor = flavor;
+        this.scoops = scoops;
+    }
+
+    includeSpoon() {
+        console.log("Here is your spoon!");
+    }
+}
